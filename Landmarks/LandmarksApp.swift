d115e3e914368e6bbc57c6b191aa4f455add7275
Taishi <taishw@uw.edu>
2022-02-04 11:34:27 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Taishi Walden on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
